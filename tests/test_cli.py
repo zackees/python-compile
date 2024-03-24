c2a59f7222b0ea38c147755bbd1b932fa7d3ed6e
zackees <z@zackees.com>
2024-03-23 18:53:12 -0700

    Add test for verifying existence of demo server binary

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,7 @@ Unit test file.
 
 import os
 import unittest
+from pathlib import Path
 
 
 class MainTester(unittest.TestCase):
@@ -20,6 +21,8 @@ class MainTester(unittest.TestCase):
         ]
         rtn = os.system(" ".join(cmd_list))
         self.assertEqual(0, rtn)
+        expected_path: Path = Path("demo_http_server.bin.gz")
+        self.assertTrue(expected_path.exists())
 
 
 if __name__ == "__main__":
