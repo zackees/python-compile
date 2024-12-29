d83962fe075f53bb6b7b2937c339f0a73e33e4aa
zackees <z@zackees.com>
2024-12-28 20:48:16 -0800

    refactor: remove unused pyMSVC dependency from build process

--- src/python_compile/native_build.py
+++ src/python_compile/native_build.py
@@ -66,11 +66,6 @@ def run_native_build(
     """Run the native windows build."""
     print("Running native build")
 
-    if sys.platform.startswith("win"):
-        import pyMSVC
-
-        pyMSVC.setup_environment()
-
     with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmp_dir:
         print(f"Creating temporary directory: {full_path(tmp_dir)}")
         full_tmp_path = os.path.abspath(tmp_dir)
